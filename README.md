1c6a12c5ff0a02155fcd9f929b9172da5c6b1d71
BipinBaj <bipinjz@gmail.com>
2022-03-24 22:39:54 +1100

    update readme , config

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# newslist
+# Description
+
+Modified version using webpack
+# Commands to run
+
+1. yarn install
+2. yarn build
+3. yarn serve
--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const HtmlWebpackPLugin = require('html-webpack-plugin')
 const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
-    mode: 'development',
+    mode: 'production',
     entry: path.resolve(__dirname, 'src/index.js'),
     output: {
         path: path.resolve(__dirname, 'dist'),
